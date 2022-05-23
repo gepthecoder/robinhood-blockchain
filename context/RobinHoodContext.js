e03f290938df4ddfd484d80710519536b597ce9c
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-23 09:24:23 +0200

    $0027: [Backend] [Moralis] [Auth]: wrap app in moralis provider, app authentication wip

--- context/RobinHoodContext.js
+++ context/RobinHoodContext.js
@@ -1,11 +1,63 @@
 import { createContext, useEffect, useState } from 'react'
+import { useMoralis } from 'react-moralis'
 
 export const RobinhoodContext = createContext()
 
 export const RobinhoodProvider = ({ children }) => {
+
+    const [currentAccount, setCurrentAccount] = useState('')
+    const [formattedAccount, setFormattedAccount] = useState('')
+
+    /* Moralis always listens in the backend for authentication activity */
+    const { isAuthenticated, authenticate, user, logout, Moralis, enableWeb3 } = useMoralis()
+
+    useEffect(() => {
+        if (isAuthenticated) {
+          const account = user.get('ethAddress')
+          let formatAccount = account.slice(0, 4) + '...' + account.slice(-4)
+          setFormattedAccount(formatAccount)
+          setCurrentAccount(account)
+        }
+    }, [isAuthenticated, enableWeb3])
+
+    /* If the user is logging in for the 1st time we want to create that user in sanity DB */
+     useEffect(() => {
+         if (!currentAccount) return
+      
+         ;(async () => {
+           const response = await fetch('/api/createUser', {
+             method: 'POST',
+             headers: {
+               'Content-Type': 'application/json',
+             },
+             body: JSON.stringify({
+               walletAddress: currentAccount,
+             }),
+           })()
+  
+           const data = await response.json()
+         })
+     }, [currentAccount])
+    
+
+    const connectWallet = () => {
+        authenticate()
+    }
+    
+    const signOut = () => {
+        console.log('Logged out')
+        logout()
+    }
+
     return (
         <RobinhoodContext.Provider
-            value={{}}
+            value={{
+                connectWallet,
+                signOut,
+                currentAccount,
+                isAuthenticated,
+                formattedAccount,
+            }}
         >
             {children}
         </RobinhoodContext.Provider>
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import '../styles/globals.css'
 import { RobinhoodProvider } from '../context/RobinHoodContext'
+import { MoralisProvider } from 'react-moralis'
 
 function MyApp({ Component, pageProps }) {
     return(
-      <RobinhoodProvider>
-        <Component {...pageProps} />
-      </RobinhoodProvider>
+      <MoralisProvider
+        serverUrl='https://frav6pbipqso.usemoralis.com:2053/server'
+        appId='EoxjtE0tkdWNEmJjzQcRSfnpekztQl7aeEFe0cxG'
+      >
+        <RobinhoodProvider>
+          <Component {...pageProps} />
+        </RobinhoodProvider>
+      </MoralisProvider>
+     
     ) 
 }
 
