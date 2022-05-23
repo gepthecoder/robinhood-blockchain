58a0574357119cfa9fe59563ef3df443c925301b
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-23 11:34:50 +0200

    $0029: [Moralis] [Auth] [Sanity]: refactor request to create user profile 🐱‍👤

--- context/RobinHoodContext.js
+++ context/RobinHoodContext.js
@@ -12,33 +12,44 @@ export const RobinhoodProvider = ({ children }) => {
     const { isAuthenticated, authenticate, user, logout, Moralis, enableWeb3 } = useMoralis()
 
     useEffect(() => {
-        if (isAuthenticated) {
-          const account = user.get('ethAddress')
-          let formatAccount = account.slice(0, 4) + '...' + account.slice(-4)
-          setFormattedAccount(formatAccount)
-          setCurrentAccount(account)
-        }
+      checkWalletConnection()
     }, [isAuthenticated, enableWeb3])
 
-    /* If the user is logging in for the 1st time we want to create that user in sanity DB */
-     useEffect(() => {
-         if (!currentAccount) return
-      
-         ;(async () => {
-           const response = await fetch('/api/createUser', {
-             method: 'POST',
-             headers: {
-               'Content-Type': 'application/json',
-             },
-             body: JSON.stringify({
-               walletAddress: currentAccount,
-             }),
-           })()
-  
-           const data = await response.json()
-         })
-     }, [currentAccount])
-    
+    const checkWalletConnection = async () => {
+      if (isAuthenticated) {
+        const address = user.get('ethAddress')
+        console.log('User is authenticated..', '🐱‍🏍', address)
+
+        let formatAccount = address.slice(0, 4) + '...' + address.slice(-4)
+        setFormattedAccount(formatAccount)
+
+        setCurrentAccount(address)
+        requestToCreateUserProfile(address)
+
+      } else {
+        /* There shouldn't be an account if the user is not created */
+        setCurrentAccount('')
+        setFormattedAccount('')
+      }
+    }
+
+    const requestToCreateUserProfile = async (walletAddress) => {
+      try {
+        console.log('Creating Sanity User..', '🤖')
+
+        await fetch(`/api/createUser`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            walletAddress: walletAddress,
+          }),
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    }
 
     const connectWallet = () => {
         authenticate()
--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -1,20 +1,23 @@
 import { client } from '../../lib/sanityClient'
 
 const createUser = async (req, res) => {
-  try {
-    const userDoc = {
-      _type: 'users',
-      _id: req.body.walletAddress,
-      userName: 'Unnamed',
-      address: req.body.walletAddress,
+    try {
+      console.log('Creating user..', '🐱‍👤')
+
+      const userDoc = {
+        _type: 'users',
+        _id: req.body.walletAddress,
+        userName: 'Unnamed',
+        address: req.body.walletAddress,
+      }
+
+      await client.createIfNotExists(userDoc)
+
+      res.status(201).send({ message: 'success' })
+    } catch (error) {
+      console.log('Something went wrong', '🐞', error)
+      res.status(500).send({ message: 'error', data: error.message })
     }
-
-    await client.createIfNotExists(userDoc)
-
-    res.status(201).send({ message: 'success' })
-  } catch (error) {
-    res.status(500).send({ message: 'error', data: error.message })
-  }
 }
 
 export default createUser
